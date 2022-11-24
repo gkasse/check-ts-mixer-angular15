d8d94b634c534df4f28d9c77749e9a20b3627dbd
gkasse <kxb3317@gmail.com>
2022-11-24 20:46:43 +0900

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Mixin } from 'ts-mixer';
+
+class Hoge {
+  echoHoge(): void {
+    console.log('hoge');
+  }
+}
+
+class Fuga {
+  echoFuga(): void {
+    console.log('fuga');
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent extends Mixin(Hoge, Fuga) implements OnInit {
   title = 'check-ts-mixer-angular15';
+
+  ngOnInit(): void {
+    this.echoHoge();
+    this.echoFuga();
+  }
 }
